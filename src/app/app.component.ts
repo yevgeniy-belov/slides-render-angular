101dd43ca6cd9426a0d00b5435ac8cedb29721a1
Yevgeniy Belov <yevgeniy.belov@microfocus.com>
2020-11-06 08:08:59 +0200

    add hotkey

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,58 +1,61 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 import { IframeCommunicationService } from './iframe-communication.service';
 // import { HotkeysService, Hotkey } from 'angular2-hotkeys';
 
 @Component({
-	selector: 'app-root',
-	templateUrl: './app.component.html',
+    selector: 'app-root',
+    templateUrl: './app.component.html',
 })
 export class AppComponent {
-	constructor(
-		private iframeSvc: IframeCommunicationService,
-		// private _hotkeysService: HotkeysService
-	) {
-		iframeSvc.addEventListener();
+    constructor(
+        private iframeSvc: IframeCommunicationService,
+        // private _hotkeysService: HotkeysService
+    ) {
+        iframeSvc.addEventListener();
+        @HostListener('document:keydown.shift.f') undo(event: any) {
+            window.top.postMessage('shift+f', '*');
+        }
 
-		// this._hotkeysService.add(
-		// 	new Hotkey('ctrl+shift+/', (event: KeyboardEvent): boolean => {
-		// 		window.top.postMessage('ctrl+shift+/', '*');
-		// 		return false;
-		// 	})
-		// );
+        // this._hotkeysService.add(
+        // 	new Hotkey('ctrl+shift+/', (event: KeyboardEvent): boolean => {
+        // 		window.top.postMessage('ctrl+shift+/', '*');
+        // 		return false;
+        // 	})
+        // );
 
-		// this._hotkeysService.add(
-		// 	new Hotkey('ctrl+alt+l', (event: KeyboardEvent): boolean => {
-		// 		window.top.postMessage('ctrl+alt+l', '*');
-		// 		return false;
-		// 	})
-		// );
+        // this._hotkeysService.add(
+        // 	new Hotkey('ctrl+alt+l', (event: KeyboardEvent): boolean => {
+        // 		window.top.postMessage('ctrl+alt+l', '*');
+        // 		return false;
+        // 	})
+        // );
 
-		// this._hotkeysService.add(
-		// 	new Hotkey('ctrl+/', (event: KeyboardEvent): boolean => {
-		// 		window.top.postMessage('ctrl+/', '*');
-		// 		return false;
-		// 	})
-		// );
+        // this._hotkeysService.add(
+        // 	new Hotkey('ctrl+/', (event: KeyboardEvent): boolean => {
+        // 		window.top.postMessage('ctrl+/', '*');
+        // 		return false;
+        // 	})
+        // );
 
-		// this._hotkeysService.add(
-		// 	new Hotkey('ctrl+alt+b', (event: KeyboardEvent): boolean => {
-		// 		window.top.postMessage('ctrl+alt+b', '*');
-		// 		return false;
-		// 	})
-		// );
+        // this._hotkeysService.add(
+        // 	new Hotkey('ctrl+alt+b', (event: KeyboardEvent): boolean => {
+        // 		window.top.postMessage('ctrl+alt+b', '*');
+        // 		return false;
+        // 	})
+        // );
 
-		// this._hotkeysService.add(
-		// 	new Hotkey('shift+f', (event: KeyboardEvent): boolean => {
-		// 		window.top.postMessage('shift+f', '*');
-		// 		return false;
-		// 	})
-		// );
+        // this._hotkeysService.add(
+        // 	new Hotkey('shift+f', (event: KeyboardEvent): boolean => {
+        // 		window.top.postMessage('shift+f', '*');
+        // 		return false;
+        // 	})
+        // );
 
-		// this._hotkeysService.add(
-		// 	new Hotkey('escape', (event: KeyboardEvent): boolean => {
-		// 		window.top.postMessage('escape', '*');
-		// 		return false;
-		// 	})
-		// );
-	}
+        // this._hotkeysService.add(
+        // 	new Hotkey('escape', (event: KeyboardEvent): boolean => {
+        // 		window.top.postMessage('escape', '*');
+        // 		return false;
+        // 	})
+        // );
+    }
 }
