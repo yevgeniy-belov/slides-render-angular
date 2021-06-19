7b6930f5960f2b5b3e98d88f17261cfc6ca2d506
Yevgeniy Belov <ugenmail@gmail.com>
2021-06-19 19:45:51 +0300

    apgrade angular to 11

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,15 +4,12 @@ import { RouterModule } from '@angular/router';
 @NgModule({
 	imports: [
 		RouterModule.forRoot([
-			{
-				path: 'slides',
-				loadChildren: () =>
-					import('./slides/slides.module').then(
-						(m) => m.SlidesModule
-					),
-			},
-			{ path: '', redirectTo: 'slides/mockups', pathMatch: 'full' },
-		]),
+    {
+        path: 'slides',
+        loadChildren: () => import('./slides/slides.module').then((m) => m.SlidesModule),
+    },
+    { path: '', redirectTo: 'slides/mockups', pathMatch: 'full' },
+], { relativeLinkResolution: 'legacy' }),
 	],
 	exports: [RouterModule],
 })
